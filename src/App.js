4951b0f574064e948de56789c639d21d549ec11d
Roberto Ezequiel Iskandarani <riskandarani@fi.uba.ar>
2022-09-12 20:05:31 -0300

    se agrega boton de error mas sonido

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import Button from "./components/Button";
 import Tablero from "./components/Tablero";
+import { MdCancel } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
+import errorSound from "./assets/error.mp3";
 
 import information from "./information";
 import { useEffect, useState } from "react";
@@ -8,11 +11,23 @@ function App() {
   const [question, setQuestion] = useState(0);
   const [counter, setCounter] = useState(0);
   const [questions, setQuestions] = useState(information[0]);
+  const [wrong, setWrong] = useState(0);
+  const [toggleWrong, setToggleWrong] = useState(false);
+  let error = new Audio(errorSound);
 
   useEffect(() => {
     setQuestions(information[question]);
     setCounter(0);
+    setWrong(0);
   }, [question]);
+
+  function showCross(params) {
+    let cross = [];
+    for (let i = 0; i < wrong; i++) {
+      cross.push(<IoClose size={"100%"} color="red" />);
+    }
+    return cross;
+  }
 
   return (
     <div
@@ -72,6 +87,50 @@ function App() {
           question={questions.question}
         />
       </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          position: "absolute",
+          top: "56%",
+          left: "92%",
+          color: "rgb(2, 66, 137)",
+          fontFamily: "Arial",
+          fontSize: 25,
+        }}
+      >
+        {wrong}
+        <MdCancel
+          style={{ marginLeft: "4%" }}
+          size={30}
+          color="rgb(2, 66, 137)"
+          onClick={() => {
+            setWrong(wrong + 1);
+            error.play();
+            setToggleWrong(true);
+            setTimeout(() => {
+              setToggleWrong(false);
+            }, 1000);
+          }}
+        />
+      </div>
+      {toggleWrong && (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          {showCross().map((cross) => cross)}
+        </div>
+      )}
     </div>
   );
 }
